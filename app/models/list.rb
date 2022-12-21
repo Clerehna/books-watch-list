f1e693820d9330b555eeb772aca789c1dd57c9f0
Charlene Nicol <charln.nicol@gmail.com>
2022-12-21 12:53:19 +0100

    fichier shared créer, ajout d'une navbar avatar et logo

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,5 +2,5 @@ class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :books, through: :bookmarks
 
-  validates :name, prensence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true
 end
