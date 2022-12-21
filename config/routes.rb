05ed2ff23ca55eaffea8fe086c8bab3337cb95b7
Charlene Nicol <charln.nicol@gmail.com>
2022-12-21 11:52:58 +0100

    creation d'un controller list, correction des routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "lists#index"
   # resources :books, only: %i[show index]
   resources :lists, only: %i[show index create new] do
-    resources :bookmarks, only: %i[show create new destroy]
+    resources :bookmarks, only: %i[create new]
   end
+
+  resources :bookmarks, only: %i[destroy]
 
 end
